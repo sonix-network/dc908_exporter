67907ee43b809c95ea0cba702a6624b5ac2c0456
Christian Svensson <blue@cmd.nu>
2024-07-07 21:29:37 +0200

    Import basic server

---
+++ go.mod
@@ -0,0 +1,18 @@
+module github.com/sonix-network/dc908_exporter
+
+go 1.22.4
+
+require (
+	github.com/golang/glog v1.2.1
+	github.com/golang/protobuf v1.5.4
+	github.com/openconfig/gnmi v0.11.0
+	google.golang.org/grpc v1.65.0
+	google.golang.org/protobuf v1.34.2
+)
+
+require (
+	golang.org/x/net v0.25.0 // indirect
+	golang.org/x/sys v0.20.0 // indirect
+	golang.org/x/text v0.15.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20240701130421-f6361c86f094 // indirect
+)
---
+++ main.go
@@ -0,0 +1,193 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"net"
+
+	pb "github.com/sonix-network/dc908_exporter/proto"
+	log "github.com/golang/glog"
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/codes"
+	"google.golang.org/grpc/peer"
+	"google.golang.org/grpc/reflection"
+)
+
+var (
+	port = flag.Int("port", 8888, "port to listen on")
+)
+
+type Server struct {
+	s         *grpc.Server
+	lis       net.Listener
+	config    *Config
+	pb.UnimplementedGNMIDialoutServer
+}
+
+type Config struct {
+	Port int64
+}
+
+func NewServer(config *Config, opts []grpc.ServerOption) (*Server, error) {
+	if config == nil {
+		panic("config not provided")
+	}
+
+	s := grpc.NewServer(opts...)
+	reflection.Register(s)
+
+	srv := &Server{
+		s:       s,
+		config:  config,
+	}
+	var err error
+	if srv.config.Port < 0 {
+		srv.config.Port = 0
+	}
+	srv.lis, err = net.Listen("tcp", fmt.Sprintf(":%d", srv.config.Port))
+	if err != nil {
+		return nil, fmt.Errorf("failed to open listener port %d: %v", srv.config.Port, err)
+	}
+	pb.RegisterGNMIDialoutServer(srv.s, srv)
+	log.V(1).Infof("Created Server on %s", srv.Address())
+	return srv, nil
+}
+
+func (srv *Server) Serve() error {
+	s := srv.s
+	if s == nil {
+		return fmt.Errorf("Serve() failed: not initialized")
+	}
+	return srv.s.Serve(srv.lis)
+}
+
+func (srv *Server) Stop() error {
+	s := srv.s
+	if s == nil {
+		return fmt.Errorf("Serve() failed: not initialized")
+	}
+	srv.s.Stop()
+	log.V(1).Infof("Server stopped on %s", srv.Address())
+	return nil
+}
+
+func (srv *Server) Address() string {
+	return srv.lis.Addr().String()
+}
+
+func (srv *Server) Port() int64 {
+	return srv.config.Port
+}
+
+func (srv *Server) Publish(stream pb.GNMIDialout_PublishServer) error {
+	ctx := stream.Context()
+
+	pr, ok := peer.FromContext(ctx)
+	if !ok {
+		return grpc.Errorf(codes.InvalidArgument, "failed to get peer from ctx")
+	}
+	if pr.Addr == net.Addr(nil) {
+		return grpc.Errorf(codes.InvalidArgument, "failed to get peer address")
+	}
+
+	c := NewClient(pr.Addr)
+	defer c.Close()
+	return c.Run(srv, stream)
+}
+
+type Client struct {
+	addr    net.Addr
+}
+
+func NewClient(addr net.Addr) *Client {
+	return &Client{
+		addr: addr,
+	}
+}
+
+func (c *Client) String() string {
+	return c.addr.String()
+}
+
+func (c *Client) Run(srv *Server, stream pb.GNMIDialout_PublishServer) (err error) {
+	defer log.V(1).Infof("Client %s shutdown", c)
+
+	if stream == nil {
+		return grpc.Errorf(codes.FailedPrecondition, "cannot start client: stream is nil")
+	}
+
+	for {
+		log.V(1).Infof("Client %s reading", c)
+		subscribeResponse, err := stream.Recv()
+		if err != nil {
+			if err == io.EOF {
+				return grpc.Errorf(codes.Aborted, "stream EOF received")
+			}
+			return grpc.Errorf(grpc.Code(err), "received error from client")
+		}
+
+		notif := subscribeResponse.GetUpdate()
+		// TODO: parse upd.Timestamp
+		prefix := ""
+		if notif.Prefix != nil {
+			for _, elem := range notif.Prefix.Elem {
+				prefix = prefix + "/" + elem.Name
+				if elem.Key != nil{
+					prefix = prefix + "["
+					first := true
+					for k, v := range elem.Key {
+						if !first {
+							prefix = prefix + ","
+						}
+						first = false
+						prefix = prefix + k + "=" + v
+					}
+					prefix = prefix + "]"
+				}
+			}
+		}
+		for _, upd := range notif.Update {
+			fqn := prefix
+			for _, elem := range upd.Path.Elem {
+				fqn = fqn + "/" + elem.Name
+				if elem.Key != nil{
+					fqn = fqn + "["
+					first := true
+					for k, v := range elem.Key {
+						if !first {
+							fqn = fqn + ","
+						}
+						first = false
+						fqn = fqn + k + "=" + v
+					}
+					fqn = fqn + "]"
+				}
+			}
+			val := upd.Val.GetJsonIetfVal()
+			log.Infof("%s: %s: %s", c.String(), fqn, val)
+		}
+	}
+	return grpc.Errorf(codes.InvalidArgument, "Exiting")
+}
+
+func (c *Client) Close() {
+}
+
+func main() {
+	flag.Parse()
+
+	opts := []grpc.ServerOption{}
+	cfg := &Config{}
+	cfg.Port = int64(*port)
+	s, err := NewServer(cfg, opts)
+	if err != nil {
+		log.Fatalf("Failed to create gNMI server: %v", err)
+	}
+
+	log.V(1).Infof("Starting RPC server on address: %s", s.Address())
+	s.Serve()
+	log.Flush()
+}
+
+
